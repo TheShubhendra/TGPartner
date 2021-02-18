1c296bed5c5205c8aab86d1233464a9f41565601
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-18 13:56:15 +0530

    Fix style

--- tgpartner/exceptions.py
+++ tgpartner/exceptions.py
@@ -1,8 +1,10 @@
 class TGPartnerException(Exception):
     """Base class of exceptions."""
+
     pass
 
 
 class EnvironmentVariableNotFound(TGPartnerException):
     """Environment Variable not found."""
+
     pass
