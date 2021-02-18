6f9d12a5e711cbf3cd3139a09163b3a9b61f2ca8
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-18 15:15:08 +0530

    Add session string generator

---
+++ session_gen.py
@@ -0,0 +1,12 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+APP_ID = input("Please Enter your APP_ID: ")
+API_HASH= input("Please Enter your API_HASH: ")
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    string = client.session.save()
+    print("Your session string is:")
+    print(string)
+    print()
+    print("You can found this on your saved messages too.")
+    client.send_message("me",f"`{string}`")
