c4b19a86ef1aad545c1ac9ec02e1c7109e7e9fa4
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-18 13:56:49 +0530

    Add more env var

--- tgpartner/config.py
+++ tgpartner/config.py
@@ -1,10 +1,11 @@
 from decouple import config
 
 
-ENV = config("ENV","development")
+ENV = config("ENV", "development")
 APP_ID = config("APP_ID")
 API_HASH = config("API_HASH")
 
 
-if ENV == "production":
-    APP_NAME = config ("APP_NAME")
+#Production variables
+APP_NAME = config("APP_NAME", None)
+STRING_SESSION = config("STRING_SESSION", None)
