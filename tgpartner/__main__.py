be53f54518cd3a4f76fef0e9f790681bd30c7b7f
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-02-18 13:58:58 +0530

    Add script to start the bot

--- tgpartner/__main__.py
+++ tgpartner/__main__.py
@@ -1,5 +1,35 @@
-from telethon import TelegramClient 
-from telethon.connection import StringSession
+import asyncio
+import logging
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
+from tgpartner.config import (
+    ENV,
+    APP_ID,
+    API_HASH,
+    STRING_SESSION,
+)
+from tgpartner.bootloader import send_success_message
 
-partner = TelegramClient(StringSession(session_name), APP_ID, API_HASH)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger()
+
+async def start(client):
+    await send_success_message(client)
+
+async def main():
+    logger.info(f"Starting bot in {ENV} environment.")
+    if ENV == "production":
+        client = TelegramClient(StringSession(STRING_SESSION), APP_ID, API_HASH)
+    else:
+        client = TelegramClient("TGPartner", APP_ID, API_HASH)
+    await client.start()
+    logger.info("Bot has been started.")
+    await start(client)
+    await client.run_until_disconnected()
+
+if __name__ == "__main__":
+    asyncio.run(main())
