d895e37ca2b8e8f8ad4bb4f254e4dfbc935bcd28
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-03-12 21:45:46 +0530

    Add option to deactivate afk

--- tgpartner/plugins/general/afk.py
+++ tgpartner/plugins/general/afk.py
@@ -23,6 +23,7 @@ import psycopg2
 from decouple import config
 
 DATABASE_URL = config("DATABASE_URL", None)
+AFK_FEATURE = bool(int(config("AFK_FEATURE",  True)))
 AFK_ACTIVATED = False
 AFK_MESSAGE = " "
 CON = psycopg2.connect(DATABASE_URL)
@@ -49,6 +50,8 @@ async def remove_afk(event):
 
 @client.on(events.NewMessage(outgoing=True, pattern="\.afk (.*)"))
 async def activate_afk(event):
+    if not AFK_FEATURE:
+        return
     global AFK_ACTIVATED, AFK_MESSAGE
     AFK_ACTIVATED = True
     AFK_MESSAGE = event.pattern_match.group(1)
