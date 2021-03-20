eaba97c691320bf9590bca7a72136a65c84e4f28
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-03-20 12:20:49 +0530

    Modify starting script

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!usr/bin/bash
+if [ -z "$DO_NOT_UPDATE_BEFORE_START" ]
+then
+     python -m tgpartner
+     exit
+fi
 if [ -z "$REPO_BRANCH" ]
 then
       REPO_BRANCH = "main"
@@ -8,3 +13,4 @@ fi
 git fetch origin $REPO_BRANCH
 git reset --hard origin/HEAD
 python -m tgpartner
+
