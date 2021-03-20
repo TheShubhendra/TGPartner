64bfac1a65952158acb6d7395f5084aebb253e80
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-03-20 12:29:00 +0530

    Fix starting script

--- start.sh
+++ start.sh
@@ -1,16 +1,14 @@
 #!usr/bin/bash
 if [ -z "$DO_NOT_UPDATE_BEFORE_START" ]
 then
-     python -m tgpartner
-     exit
+    if [ -z "$REPO_BRANCH" ]
+    then
+        REPO_BRANCH = "main"
+    else
+        echo "Fetching from custom branch $REPO_BRANCH"
+    fi
+    git fetch origin $REPO_BRANCH
+    git reset --hard origin/HEAD
 fi
-if [ -z "$REPO_BRANCH" ]
-then
-      REPO_BRANCH = "main"
-else
-      echo "Fetching from custom branch $REPO_BRANCH"
-fi
-git fetch origin $REPO_BRANCH
-git reset --hard origin/HEAD
 python -m tgpartner
 
